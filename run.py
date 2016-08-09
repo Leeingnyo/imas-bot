349f08bffab6fdcc911ec83dafcb803f18eba9c8
Leeingnyo <dyd1928@naver.com>
2016-08-10 02:58:38 +0900

    교체

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from ircbot import Bot
 from crawler import Crawler
 from crawler_method.dccrawler import crawl_dcinside
+from crawler_method.dccrawler_censored import crawl_dcinside_censored
 from crawler_method.invencrawler import crawl_inven
 from crawler_method.rulicrawler import crawl_ruliweb
 from crawler_method.shuneicrawler import crawl_shunei
@@ -8,7 +9,7 @@ from crawler_method.yayocrawler import crawl_yayo
 
 bot = Bot.Instance()
 crawler_list = [
-    Crawler(crawl_dcinside, queue=bot.queue),
+    Crawler(crawl_dcinside_censored, queue=bot.queue),
     Crawler(crawl_inven, queue=bot.queue),
     Crawler(crawl_ruliweb, queue=bot.queue),
     Crawler(crawl_shunei, queue=bot.queue),

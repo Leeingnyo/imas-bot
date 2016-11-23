32c68f23f5213a9dd926a2ad7758de0039424756
Leeingnyo <dyd1928@naver.com>
2016-11-23 23:30:35 +0900

    765 마이너 갤러리 추가

---
+++ crawler_method/dccrawler_765_censored.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def crawl_dcinside_765_censored():
+    keywords = ['핫산', '그림', '만화']
+    r = requests.get('http://gall.dcinside.com/mgallery/board/lists/?id=765pro&page=1&exception_mode=recommend')
+    html = r.content
+    parsed = BeautifulSoup(html, 'html.parser')
+    tbody = parsed.tbody
+    articles = tbody.find_all(class_='tb')
+    reformed = []
+    for article in articles:
+        if article.find(class_='t_notice').string == '공지':
+            continue
+        link = 'http://gall.dcinside.com/mgallery/board/view/?id=765pro&' + article.find(class_='t_subject').a['href'].split('&')[1]
+        title = article.find(class_='t_subject').a.string
+        P = False
+        for keyword in keywords:
+            P = P or keyword in title
+        if not P:
+            continue
+        date = article.find(class_='t_date')['title']
+        reformed.append(('765념', title, link, date))
+    return reformed
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from ircbot import Bot
 from crawler import Crawler
 from crawler_method.dccrawler import crawl_dcinside
 from crawler_method.dccrawler_censored import crawl_dcinside_censored
+from crawler_method.dccrawler_765 import crawl_dcinside_765
+from crawler_method.dccrawler_765_censored import crawl_dcinside_765_censored
 from crawler_method.invencrawler import crawl_inven
 from crawler_method.rulicrawler import crawl_ruliweb
 from crawler_method.shuneicrawler import crawl_shunei
@@ -12,6 +14,7 @@ from setting import setting_chan_list
 bot = Bot.Instance()
 crawler_list = [
     Crawler(crawl_dcinside_censored, queue=bot.queue),
+    Crawler(crawl_dcinside_765_censored, queue=bot.queue),
     Crawler(crawl_inven, queue=bot.queue),
     Crawler(crawl_ruliweb, queue=bot.queue),
     Crawler(crawl_shunei, queue=bot.queue),
@@ -19,6 +22,7 @@ crawler_list = [
 ]
 specific_crawler_list = [
     Crawler(crawl_dcinside, queue=bot.queue, chan=[setting_chan_list[1][0]]),
+    Crawler(crawl_dcinside_765, queue=bot.queue, chan=[setting_chan_list[1][0]]),
 ]
 for crawler in crawler_list + specific_crawler_list:
     crawler.setDaemon(True)
